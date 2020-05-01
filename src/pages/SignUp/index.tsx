8cb0652776f980c03cb512a4cab4151e7d44e96c
Patrik Oliveira <patrik.oliveira@rarolabs.com.br>
2020-05-01 00:28:41 -0300

    feat: Melhorias de usabilidade para inputs

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -20,7 +20,7 @@ const SignUp: React.FC = () => {
       <Content>
         <img src={logoImg} alt="GoBarber" />
 
-        <Form initialData={{ name: 'Patrik Oliveira' }} onSubmit={handleSubmit}>
+        <Form onSubmit={handleSubmit}>
           <h1>Faça seu Cadastro</h1>
 
           <Input name="name" icon={FiUser} placeholder="Nome" />
